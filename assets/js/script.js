434f5e8e4a5836b36378b1d5efdc8b6f94e56151
MichaelRempe <michaeljrempe@gmail.com>
2020-01-17 13:39:23 -0500

    TM data formated to comunicate with Map Quest queries

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,37 @@
-$(document).ready(function() {
+$(document).ready(function () {
   //get user's current location
   //pass current location into map query
-  
+
   //pass location and current time into ticketmaster query
   //get array of objects of events
-  
+
   //check location of events
   //check current location
   //if user can reach destination in time,
   //display event
   var viableEvents = [];
-  
+
   //Global lat and long retrieved from user with permission
-  var lat="";
-  var lon ="";
+  var lat = "";
+  var lon = "";
   //queries
   var searchAddress;
   var searchCity;
   var searchState;
   var searchTime;
   var searchCategory;
-  
+
+  //mapQuest route queries
+  var startPoint;
   //temp
   var searchTime_24;
-  
+
   var travelTime;
   var locationsDropdown = ['Washington', 'New York City', 'Philadelphia'];
+  var locationAttributes = ["DC", "NY", "PA"];
   var timeDropdown = [];
   var categoryDropdown = ['Sports', 'Music', 'Theater', 'Dance', 'Other'];
-  
+
   // Get DOM elements
   var searchBtn = $('#search-btn');
   var inputDOM = $('#search-input');
@@ -37,9 +40,10 @@ $(document).ready(function() {
   var timeMenuDOM = $('.time-menu');
   var categoryMenuDOM = $('.category-menu');
   var eventsListDOM = $('.events-list');
-  
+
   //HTML5 retrieve permission from user to get current location (lat and long)
   function getCurrentLocation() {
+    
     function success(position) {
       //if permission granted set call setAddress to convert lat and long to street adresss
       lat = position.coords.latitude;
@@ -49,8 +53,10 @@ $(document).ready(function() {
     function denied() {
       // if permission is denied set address query to "Arlington" and call main (set time and category)
       console.log('Unable to retrieve your location');
+      searchAddress="";
       searchCity = "Washington";
       searchState = "DC";
+      startPoint = searchCity + ", " + searchState;
       main();
     }
     if (!navigator.geolocation) {
@@ -60,10 +66,10 @@ $(document).ready(function() {
       navigator.geolocation.getCurrentPosition(success, denied);
     }
   }
-  function setAddress(){
+  function setAddress() {
     getStreet();
   }
-  function setTime(){
+  function setTime() {
     //Collects current Time and rounds to nearest 30 min window
     var start = moment();
     var interval = 30 - (start.minute() % 30);
@@ -72,181 +78,191 @@ $(document).ready(function() {
     searchTime = day.format();
     // additional time to ISO 8601 format +24 hours
     searchTime_24 = moment(day).add(24, "hours");
-    searchTime_24 =searchTime_24.format();
-    
+    searchTime_24 = searchTime_24.format();
+
   }
-  function setEvent(){
+  function setEvent() {
     searchCategory = "";
   }
-  function main(){
+  function main() {
     //inital set adress achieve from current location call on request prompt to user
     setTime();
     setEvent();
     search_tmaster();
-  
+
   }
-  
-    // Current Location Street Address AJAX from lat and lon
-    function getStreet() {
-      var apikey = '8iMbHQoKISbmKAynwHsO7ZlMhuPhWgtu';
-      $.ajax({
-        url: 'http://www.mapquestapi.com/geocoding/v1/reverse',
-        dataType: 'json',
-        method: 'GET',
-        data: {
-          location: lat + "," + lon,
-          key: apikey,
-        }
-      }).then(function(response) {
-        searchAddress = response.results[0].locations[0].street;
-        searchCity = response.results[0].locations[0].adminArea5;
-        searchState = response.results[0].locations[0].adminArea3;
-        main();
-        // getRoute(searchAddress);
-      });
-    }
-    function search_tmaster(){
-      accessTicketMaster();
-    }
-    // Ticket Master AJAX
-    function accessTicketMaster(){
-      $.ajax({
-        url: "https://app.ticketmaster.com/discovery/v2/events.json?",
-        method: "GET",
-        data:{
-            apikey: "xB4pwlx2qXShKTb5vBvUcL98KBiIpsdp",
-            city: searchCity,
-            stateCode: searchState,
-            countryCode: "US",
-            keyword: searchCategory,
-            startDateTime: searchTime,
-            endDateTime: searchTime_24
-        }
+
+  // Current Location Street Address AJAX from lat and lon
+  function getStreet() {
+    var apikey = '8iMbHQoKISbmKAynwHsO7ZlMhuPhWgtu';
+    $.ajax({
+      url: 'http://www.mapquestapi.com/geocoding/v1/reverse',
+      dataType: 'json',
+      method: 'GET',
+      data: {
+        location: lat + "," + lon,
+        key: apikey,
+      }
+    }).then(function (response) {
+      searchAddress = response.results[0].locations[0].street;
+      searchCity = response.results[0].locations[0].adminArea5;
+      searchState = response.results[0].locations[0].adminArea3;
+      startPoint = searchAddress + ", " + searchCity + ", " + searchState;
+
+      main();
+      // getRoute(searchAddress);
+    });
+  }
+  function search_tmaster() {
+    accessTicketMaster();
+  }
+  // Ticket Master AJAX
+  function accessTicketMaster() {
+    $.ajax({
+      url: "https://app.ticketmaster.com/discovery/v2/events.json?",
+      method: "GET",
+      data: {
+        apikey: "xB4pwlx2qXShKTb5vBvUcL98KBiIpsdp",
+        city: searchCity,
+        stateCode: searchState,
+        countryCode: "US",
+        keyword: searchCategory,
+        startDateTime: searchTime,
+        endDateTime: searchTime_24
+      }
     }).done(function (response) {
-        console.log(response);
-        checkEvents(response);
-    });
-    }
-    // Travel time AJAX from currentAddress
-    function getRoute(searchAddress){
-      $.post("http://www.mapquestapi.com/directions/v2/routematrix?key=XBEFbd4lAqBbeNeE8QyUcxIbYlnlARLz",
-        "json=" + JSON.stringify({
-          'locations': [searchAddress, 'Washington, DC'],
-          'options': { 'allToAll': false }
-        }),
-        function (response) {
-          console.log(response);
-          travelTime = response.time[1];
-          // Round travelTime into nearest minute
-          var travelMins = Math.round(travelTime/60);
-          console.log(travelMins);
-        }, "json");
-    }
-  
-    //animate search btn makes everything slide up
-    searchBtn.on('click', function() {
-      // $('.header').slideUp();
-      $('.header').animate({
-        'marginTop' : "-120px"
-      });
-      searchbarDOM.animate({
-        'marginTop' : "1.5em"
-      }, "slow");
-    });
-  
-    makeDropdowns();
-  
-    // Make dropdown elements
-    function makeDropdowns() {
-  
-      for( var i = 0; i < locationsDropdown.length; i++ ){
-        var locationMenuItem = $('<a>');
-        locationMenuItem.addClass('dropdown-item');
-        locationMenuItem.addClass('location-item');
-        var itemText = locationsDropdown[i];
-        locationMenuItem.text(itemText);
-        locationMenuItem.on('click', function() {
-          searchCity = $(this).text();
-          inputDOM.attr('placeholder', searchCity);
-          console.log(searchCity);
-          // set search state to nothing for the time being
-          searchState ="";
-          search_tmaster();
-        });
-  
-        locationMenuDOM.append(locationMenuItem);
-      }
-      // Get hours, push to timeDropdown array
-      // military
-      var currentHour = moment().format('H');
-      console.log(currentHour);
-      // if the currentHour (in military time) is in the morning,
-      // add up to 12 hours
-      if( parseInt(currentHour) < 12){
-        // while currentHour < 23, add 1
-        for( var i = 0; (parseInt(currentHour) + i) < 23; i++ ){
-          currentHour = moment().add(i, 'h').format('ha');
-  
-          timeDropdown.push(currentHour);
-          console.log('am');
-        }
-      } else if( parseInt(currentHour) >= 12 ){
-        for( var i = 0; (parseInt(currentHour) + i) < 20; i++ ){
-          currentHour = moment().add(i, 'h').format('ha');
-          timeDropdown.push(currentHour);
-          console.log('pm');
-        }
-      }
-  
-      for( var i = 0; i < timeDropdown.length; i++ ){
-        var timeMenuItem = $('<a>');
-        timeMenuItem.addClass('dropdown-item');
-        timeMenuItem.addClass('time-item');
-        var itemText = timeDropdown[i];
-        timeMenuItem.text(itemText);
-        timeMenuItem.attr('iso86', moment().add(i, 'h').format());
-        timeMenuItem.on('click', function() {
-          searchTime = $(this).text();
-          inputDOM.attr('placeholder', searchTime);
-          console.log(searchTime);
-          search_tmaster();
-        });
-  
-        timeMenuDOM.append(timeMenuItem);
-      }
-      for( var i = 0; i < categoryDropdown.length; i++ ){
-        var categoryMenuItem = $('<a>');
-        categoryMenuItem.addClass('dropdown-item');
-        categoryMenuItem.addClass('category-item');
-        var itemText = categoryDropdown[i];
-        categoryMenuItem.text(itemText);
-        categoryMenuItem.on('click', function() {
-          searchCategory = $(this).text();
-          inputDOM.attr('placeholder', searchCategory);
-          console.log(searchCategory);
-          search_tmaster();
-        });
-  
-        categoryMenuDOM.append(categoryMenuItem);
-      }
-    }
-    getCurrentLocation();
+      console.log(response);
+      checkEvents(response);
 
-  function checkEvents(response){
-    for( var i = 0; i < response._embedded.events.length; i++ ){
-      if((searchTime_24 + travelTime) < response._embedded.events[i].dates.start.dateTime) {
-        var event = response._embedded.events[i];
-        viableEvents.push(event);
-        renderEvents();
-      }
+    });
+  }
+  function checkEvents(response) {
+    for (var i = 0; i < response._embedded.events.length; i++) {
+      var eventTime = response._embedded.events[i].dates.start.dateTime;
+      var eventAddress = response._embedded.events[i]._embedded.venues[0].name + " " +
+        response._embedded.events[i]._embedded.venues[0].address.line1 + ", " +
+        response._embedded.events[i]._embedded.venues[0].city.name + ", " +
+        response._embedded.events[i]._embedded.venues[0].state.stateCode;
+        console.log(startPoint);
+        console.log(eventAddress);
+      getRoute(startPoint, eventAddress);
+
+      // if((searchTime_24 + travelTime) < response._embedded.events[i].dates.start.dateTime) {
+      //   var event = response._embedded.events[i];
+      //   viableEvents.push(event);
+      //   renderEvents();
+      // }
     }
   }
+  // Travel time AJAX from currentAddress
+  function getRoute(start, end) {
+    $.post("http://www.mapquestapi.com/directions/v2/routematrix?key=XBEFbd4lAqBbeNeE8QyUcxIbYlnlARLz",
+      "json=" + JSON.stringify({
+        'locations': [start, end],
+        'options': { 'allToAll': false }
+      }),
+      function (response) {
+        console.log(response);
+        travelTime = response.time[1];
+        // Round travelTime into nearest minute
+        var travelMins = Math.round(travelTime / 60);
+        console.log(travelMins);
+      }, "json");
+  }
 
+  //animate search btn makes everything slide up
+  searchBtn.on('click', function () {
+    // $('.header').slideUp();
+    $('.header').animate({
+      'marginTop': "-120px"
+    });
+    searchbarDOM.animate({
+      'marginTop': "1.5em"
+    }, "slow");
+  });
+
+  // Make dropdown elements
+  function makeDropdowns() {
+
+    for (var i = 0; i < locationsDropdown.length; i++) {
+      var locationMenuItem = $('<a>');
+      locationMenuItem.addClass('dropdown-item');
+      locationMenuItem.addClass('location-item');
+      var itemText = locationsDropdown[i];
+      locationMenuItem.text(itemText);
+      locationMenuItem.attr("state-code", locationAttributes[i]);
+      locationMenuItem.on('click', function () {
+        searchCity = $(this).text();
+        searchState = $(this).attr("state-code");
+        startPoint = searchCity+", "+searchState;
+        inputDOM.attr('placeholder', startPoint);
+        console.log(startPoint);
+        // set search state to nothing for the time being
+        searchState = "";
+        search_tmaster();
+      });
+
+      locationMenuDOM.append(locationMenuItem);
+    }
+    // Get hours, push to timeDropdown array
+    // military
+    var currentHour = moment().format('H');
+    console.log(currentHour);
+    // if the currentHour (in military time) is in the morning,
+    // add up to 12 hours
+    if (parseInt(currentHour) < 12) {
+      // while currentHour < 23, add 1
+      for (var i = 0; (parseInt(currentHour) + i) < 23; i++) {
+        currentHour = moment().add(i, 'h').format('ha');
+
+        timeDropdown.push(currentHour);
+        console.log('am');
+      }
+    } else if (parseInt(currentHour) >= 12) {
+      for (var i = 0; (parseInt(currentHour) + i) < 20; i++) {
+        currentHour = moment().add(i, 'h').format('ha');
+        timeDropdown.push(currentHour);
+        console.log('pm');
+      }
+    }
+
+    for (var i = 0; i < timeDropdown.length; i++) {
+      var timeMenuItem = $('<a>');
+      timeMenuItem.addClass('dropdown-item');
+      timeMenuItem.addClass('time-item');
+      var itemText = timeDropdown[i];
+      timeMenuItem.text(itemText);
+      timeMenuItem.attr('iso86', moment().add(i, 'h').format());
+      timeMenuItem.on('click', function () {
+        searchTime = $(this).text();
+        inputDOM.attr('placeholder', searchTime);
+        console.log(searchTime);
+        search_tmaster();
+      });
+
+      timeMenuDOM.append(timeMenuItem);
+    }
+    for (var i = 0; i < categoryDropdown.length; i++) {
+      var categoryMenuItem = $('<a>');
+      categoryMenuItem.addClass('dropdown-item');
+      categoryMenuItem.addClass('category-item');
+      var itemText = categoryDropdown[i];
+      categoryMenuItem.text(itemText);
+      categoryMenuItem.on('click', function () {
+        searchCategory = $(this).text();
+        inputDOM.attr('placeholder', searchCategory);
+        console.log(searchCategory);
+        search_tmaster();
+      });
+
+      categoryMenuDOM.append(categoryMenuItem);
+    }
+  }
   //render events
   function renderEvents() {
     console.log(viableEvents);
     //make DOM elements for each array item
-    for( var i = 0; i < viableEvents.length; i++ ){
+    for (var i = 0; i < viableEvents.length; i++) {
       console.log("it's happening");
       var eventNameDOM = $('<h2>');
       var eventImageDOM = $('<img>');
@@ -271,4 +287,7 @@ $(document).ready(function() {
       eventsListDOM.append(eventURL);
     }
   }
+
+  makeDropdowns();
+  getCurrentLocation();
 })//document ready end point 
